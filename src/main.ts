05a964db4f62193c95b8b04ce8ec10c36c6cd482
Leonardo Lescano <leolescanomdq@gmail.com>
2026-08-26 16:09:50 -0300

    feat: cria interfaces de pokemon

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,11 @@
-console.log("Pokédex TypeScript Lite iniciado.");
+import { PokemonResumo } from "./models/Pokemon";
+
+const pokemonTeste: PokemonResumo = {
+  id: 25,
+  nome: "pikachu",
+  tipos: ["electric"],
+  altura: 4,
+  peso: 60
+};
+
+console.log(pokemonTeste);
